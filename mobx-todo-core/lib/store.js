e28eebd98c06169d42ca51c9173533af45bd8cdd
Roy Chen <roy850323@gmail.com>
2022-11-20 14:49:10 +0800

    Using todo-api in mobx-todo-core

--- mobx-todo-core/lib/store.js
+++ mobx-todo-core/lib/store.js
@@ -1,45 +1,82 @@
 'use strict';
 
 import { makeObservable, observable, action } from 'mobx';
+import {
+  fetchTodoListApi,
+  addItemApi,
+  updateItemApi,
+  deleteItemApi,
+} from 'todo-api';
 
 class Store {
   list = [];
+  loading = false;
 
   constructor() {
     console.log('TodoStore constructor');
     makeObservable(this, {
       list: observable,
+      loading: observable,
+      fetchTodoList: action,
       addItem: action,
       toggleItem: action,
       deleteItem: action,
     });
   }
 
-  addItem(content) {
-    const data = {
-      id: new Date().getTime(),
-      content,
-      isDone: false,
-    };
-    this.list.push(data);
+  fetchTodoList() {
+    this.loading = true;
+    fetchTodoListApi()
+      .then((response) => {
+        this.list = response;
+      })
+      .finally(() => {
+        this.loading = false;
+      });
   }
 
-  toggleItem(id) {
-    const newList = this.list.map((item) => {
-      if (item.id === id) {
-        return {
-          ...item,
-          isDone: !item.isDone,
-        };
-      }
-      return item;
-    });
-    this.list = newList;
+  addItem(title) {
+    this.loading = true;
+
+    addItemApi(title)
+      .then((response) => {
+        this.list.unshift(response);
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  }
+
+  toggleItem(data) {
+    this.loading = true;
+
+    const postData = {
+      ...data,
+      completed: !data.completed,
+    };
+    updateItemApi(postData)
+      .then((response) => {
+        const index = this.list.findIndex((todo) => todo.id === response.id);
+        if (index !== -1) {
+          this.list.splice(index, 1, response);
+        }
+      })
+      .finally(() => {
+        this.loading = false;
+      });
   }
 
   deleteItem(id) {
-    const newList = this.list.filter((item) => item.id !== id);
-    this.list = newList;
+    this.loading = true;
+
+    deleteItemApi(id)
+      .then(() => {
+        const newList = this.list.filter((item) => item.id !== id);
+        this.list = newList;
+      })
+      .finally(() => {
+        this.loading = false;
+      });
   }
 }
 
