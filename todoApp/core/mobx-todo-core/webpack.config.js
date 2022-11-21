b26446be50fb6f00a2bf14c83530585db1051677
Roy Chen <roy850323@gmail.com>
2022-11-21 23:47:08 +0800

    mobx-todo-core build to library

---
+++ todoApp/core/mobx-todo-core/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'store.js'),
+  output: {
+    filename: 'store.js',
+    library: {
+      type: 'module',
+    },
+  },
+  experiments: {
+    outputModule: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?(js)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
