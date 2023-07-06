d79a58307e8a2f2067c68e87fa0d67ab6364e48a
Roy Chen <roy850323@gmail.com>
2023-07-06 14:58:10 +0800

    remove builded components

--- micro-frontend-ui/src/App.js
+++ micro-frontend-ui/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { Button } from './components'
-import { Button as BuildButton } from '../dist'
+// import { Button as BuildButton } from '../dist'
 
 const App = () => {
-  console.log('Button', Button)
-  console.log('BuildButton', BuildButton)
   return (
     <div>
       <h1>Micro frontend UI</h1>
       <Button>Normal</Button>
       <Button primary>Primary</Button>
-      <BuildButton>Normal</BuildButton>
-      <BuildButton primary>Primary</BuildButton>
+      {/* <BuildButton>Normal</BuildButton>
+      <BuildButton primary>Primary</BuildButton> */}
     </div>
   );
 };
